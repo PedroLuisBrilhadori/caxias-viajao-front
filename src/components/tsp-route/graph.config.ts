ba3d1f238aa6d6a58e0feff96fcb66d4535f1d79
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-05-17 07:34:21 -0300

    refactor: ajutas configurações dos gráfos

--- src/components/tsp-route/graph.config.ts
+++ src/components/tsp-route/graph.config.ts
@@ -1,12 +1,59 @@
-import { GraphConfiguration, GraphLink, GraphNode } from "react-d3-graph";
+import { TSPGraphConfig } from ".";
 
-export const graphConfig: Partial<GraphConfiguration<GraphNode, GraphLink>> = {
-  height: 800,
-  width: 800,
-  directed: true,
-  node: {
-    color: "red",
-    size: 600,
-    fontSize: 16,
-  },
+export const nodeConfig = {
+  color: "red",
+  fontColor: "black",
+  fontSize: 8,
+  fontWeight: "normal",
+  opacity: 1,
+  renderLabel: true,
+  size: 600,
+  strokeColor: "none",
+  strokeWidth: 1.5,
+  symbolType: "circle",
 };
+
+export const linkConfig = {
+  color: "#d3d3d3",
+  fontColor: "black",
+  fontSize: 24,
+  fontWeight: "normal",
+  mouseCursor: "pointer",
+  opacity: 1,
+  semanticStrokeWidth: true,
+  strokeWidth: 3,
+  strokeLinecap: "butt",
+};
+
+export const d3Config = {
+  alphaTarget: 0.05,
+  gravity: -100,
+  linkLength: 100,
+  linkStrength: 1,
+  disableLinkForce: false,
+};
+
+export const config = {
+  automaticRearrangeAfterDropNode: false,
+  collapsible: false,
+  directed: true,
+  focusAnimationDuration: 0.75,
+  focusZoom: 1,
+  freezeAllDragEvents: false,
+  height: 600,
+  width: 600,
+  highlightDegree: 1,
+  highlightOpacity: 1,
+  linkHighlightBehavior: false,
+  maxZoom: 8,
+  minZoom: 2,
+  nodeHighlightBehavior: false,
+  panAndZoom: false,
+  staticGraph: false,
+  staticGraphWithDragAndDrop: false,
+  d3: d3Config,
+  node: nodeConfig,
+  link: linkConfig,
+};
+
+export const graphConfig: TSPGraphConfig = config;
--- src/components/tsp-route/types.ts
+++ src/components/tsp-route/types.ts
@@ -1,11 +1,18 @@
-import { GraphData, GraphLink } from "react-d3-graph";
+import {
+  GraphConfiguration,
+  GraphData,
+  GraphLink,
+  NodeLevelNodeConfiguration,
+} from "react-d3-graph";
 
-export type TspRoute = {
+export interface TspRoute extends Partial<NodeLevelNodeConfiguration> {
   id: string;
   targetId: string;
-};
+}
 
 export type TSPGraphData = GraphData<TspRoute, GraphLink>;
+
+export type TSPGraphConfig = Partial<GraphConfiguration<TspRoute, GraphLink>>;
 
 export type TSPRoutesComponentProps = {
   locations: TspRoute[];
